b00945759ebe6bffac6ab51218b1e6e6dd91c3c2
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-10-12 13:46:20 +0530

    edit btn added if user is logged in and blog created by that user

--- blog_posting_web_app_fe/src/Pages/Home/Home.jsx
+++ blog_posting_web_app_fe/src/Pages/Home/Home.jsx
@@ -5,33 +5,45 @@ import styles from "./Home.module.css"
 import './Blog.css';
 import { useOutletContext } from 'react-router-dom';
 import { AuthContext } from '../../utils/AuthContex';
+import { useSnackbar } from "notistack";
+
 export default function Home() {
+  const { enqueueSnackbar } = useSnackbar();
 
   const [posts, setPosts] = useState([])
   const userLocation = useOutletContext()
   const context = useContext(AuthContext)
-  console.log(userLocation.userLocation)
+  // console.log(context?.user.user)
+  // console.log(userLocation.userLocation)
   useEffect(() => {
     const fetchPosts = async () => {
-      let result = await fetchBlogs()
-      console.log(result)
-      setPosts(result)
+      try {
+        let result = await fetchBlogs();
+        // console.log(result)
+        setPosts(result);
+      } catch (err) {
+        if (err.response && err.response.data) {
+          enqueueSnackbar(err.response.data.message, { variant: "error" });
+        }
+        else {
+          enqueueSnackbar("Something went wrong. Check that the backend is running, reachable and returns valid JSON.", { variant: "error" });
+        }
+      }
     }
-    fetchPosts()
-  }, [])
+
+    fetchPosts();
+  }, [context.user])
+
   useEffect(() => {
-    let postLayoutHtml = `
-    <div style="min-height: 100px;width: 80%; background-color: aliceblue ;margin: 5% auto;">
-      </div>`
     let htmlData = ``
     posts.forEach((ele) => htmlData +=
-      `
-      <div style="min-height: 100px;width: 80%; background-color: aliceblue;margin: 1% auto; padding: 1% 5%;overflow: scroll;scrollbar-width: none;border:1px solid lightblue ">
+      `<div style="min-height: 100px;width: 80%; background-color: aliceblue;margin: 1% auto; padding: 1% 5%;overflow: scroll;scrollbar-width: none;border:1px solid lightblue ">
+      ${context?.user?.isLoggedIn && ele?.email == context?.user?.user?.email ? '<button class="editbtn" id="editBtn">Edit</button>' : ''}
       <h1 style="text-align:center;">${ele.title}</h1>
     ${ele.content}
       </div>`)
-    console.log(htmlData)
-    document.getElementById("postContainer").innerHTML = htmlData
+    // console.log(htmlData)
+    document.getElementById("postContainer").innerHTML = htmlData;
   }
     , [posts])
 
